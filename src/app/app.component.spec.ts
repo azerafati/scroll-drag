7197e89e6fea0b62914d3cfc414dba9705dcdde6
Alireza Zerafati <azerafati@gmail.com>
2023-03-18 11:15:53 +0100

    feat(demo-page): add demo page using GitHub actions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('scroll-drag app is running!');
+    expect(compiled.querySelector('h2#title')?.textContent).toContain('Scroll Drag');
   });
 });
