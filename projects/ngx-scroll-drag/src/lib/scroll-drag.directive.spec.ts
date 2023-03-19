07824119267d8c58525b429447e97ef8bd27bb4e
Alireza Zerafati <azerafati@gmail.com>
2023-03-19 16:50:21 +0100

    feat(unit-test): switch to jest from karma

--- projects/ngx-scroll-drag/src/lib/scroll-drag.directive.spec.ts
+++ projects/ngx-scroll-drag/src/lib/scroll-drag.directive.spec.ts
@@ -1,8 +1,42 @@
-import { ScrollDragDirective } from './scroll-drag.directive';
+import { ScrollDragDirective } from './scroll-drag.directive'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Component, DebugElement, ElementRef, Renderer2 } from '@angular/core'
+import { By } from '@angular/platform-browser'
+
+export class MockElementRef extends ElementRef {
+  constructor() {
+    super(null)
+  }
+}
+
+@Component({
+  template: `
+    <div ngxScrollDrag></div>`
+})
+class TestComponent {
+}
 
 describe('ScrollDragDirective', () => {
+  let directiveDebugElement: DebugElement
+  let directive: ScrollDragDirective
+  let fixture: ComponentFixture<TestComponent>
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestComponent, ScrollDragDirective],
+      providers: [
+        {provide: ElementRef, useClass: MockElementRef},
+        Renderer2,
+        ScrollDragDirective,
+      ],
+    }).createComponent(TestComponent)
+    fixture.detectChanges()
+    directive = fixture.componentInstance as ScrollDragDirective
+    directiveDebugElement = fixture.debugElement.query(By.directive(ScrollDragDirective))
+  })
+
   it('should create an instance', () => {
-    const directive = new ScrollDragDirective({},{});
-    expect(directive).toBeTruthy();
-  });
+    expect(directive).toBeTruthy()
+    expect(directiveDebugElement).toBeTruthy()
+  })
 });
