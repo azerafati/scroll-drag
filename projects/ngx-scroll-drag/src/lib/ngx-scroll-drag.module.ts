b1e3ea8ed5458f2a77186bc7124b0f38d1389e19
Alireza Zerafati <azerafati@gmail.com>
2025-01-29 18:42:24 +0100

    chore(ng): update angular version to 19

--- projects/ngx-scroll-drag/src/lib/ngx-scroll-drag.module.ts
+++ projects/ngx-scroll-drag/src/lib/ngx-scroll-drag.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core'
-import { ScrollDragDirective } from './scroll-drag.directive'
-import { ScrollShadowComponent } from './scroll-shadow/scroll-shadow.component'
-import { JsonPipe, NgIf } from "@angular/common"
+import {NgModule} from '@angular/core'
+import {ScrollDragDirective} from './scroll-drag.directive'
+import {ScrollShadowComponent} from './scroll-shadow/scroll-shadow.component'
+import {NgIf} from "@angular/common"
 
 @NgModule({
-  declarations: [
-    ScrollDragDirective,
-    ScrollShadowComponent
-  ],
-  imports: [
-    JsonPipe,
-    NgIf
-  ],
+  declarations: [ScrollDragDirective, ScrollShadowComponent],
+  imports: [NgIf],
   exports: [ScrollDragDirective, ScrollShadowComponent]
 })
 export class NgxScrollDragModule {
